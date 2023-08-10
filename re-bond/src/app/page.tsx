a9645d4906b64d67ece27b8b52e6054e397c56d1
coa3 <lovecoal928@gmail.com>
2023-08-10 15:27:01 +0900

    chore: tailwindcssのインストール

--- re-bond/src/app/page.tsx
+++ re-bond/src/app/page.tsx
@@ -1,9 +1,10 @@
-import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-
+    <main>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </main>
   )
 }
