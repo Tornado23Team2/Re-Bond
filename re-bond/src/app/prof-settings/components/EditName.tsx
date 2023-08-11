32629e9bb9d1e0a189580d17b13b69751b12541c
coa3 <lovecoal928@gmail.com>
2023-08-12 05:09:18 +0900

    feat: 名前変更フォームの追加

---
+++ re-bond/src/app/prof-settings/components/EditName.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const EditName = () => {
+  return (
+    <>
+      <label>
+        <p>ユーザー名</p>
+        <input defaultValue={'hogehoge'} placeholder="ユーザー名"/>
+      </label>
+    </>
+  )
+}
+
+export default EditName
