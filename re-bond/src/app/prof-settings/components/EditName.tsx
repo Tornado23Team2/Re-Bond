0e61b7600791af6aa54e5c6e7216c81cd1c238b5
coa3 <lovecoal928@gmail.com>
2023-08-13 21:23:34 +0900

    feat: 名前とプロフィール入力フォーム

--- re-bond/src/app/prof-settings/components/EditName.tsx
+++ re-bond/src/app/prof-settings/components/EditName.tsx
@@ -1,12 +1,27 @@
-import React from 'react'
+'use client'
+import { Flex } from '@/components/elements/box/Flex'
+import React, { useState } from 'react'
 
 const EditName = () => {
+
   return (
     <>
-      <label>
-        <p>ユーザー名</p>
-        <input defaultValue={'hogehoge'} placeholder="ユーザー名"/>
-      </label>
+      <Flex
+        align='center'
+        justify='center'
+        className=''
+      >
+        <label htmlFor='name'>
+          <p>ユーザー名</p>
+          <input
+            type='text'
+            id='name'
+            defaultValue={'hogehoge'}
+            placeholder="ユーザー名"
+            className='p-3 rounded-sm bg-gray-200'
+          />
+        </label>
+      </Flex>
     </>
   )
 }
