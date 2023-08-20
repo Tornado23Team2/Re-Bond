6ce1322318e1f361400cbeeb416fb25121912e35
coa3 <lovecoal928@gmail.com>
2023-08-21 00:05:43 +0900

    feat: ホーム画面のコンポーネント追加

--- re-bond/src/app/home/page.tsx
+++ re-bond/src/app/home/page.tsx
@@ -1,19 +1,20 @@
 import { Flex } from '@/components/elements/box/Flex'
 import Footer from '@/components/layouts/footer/Footer'
 import React from 'react'
+import UserList from './components/UserList'
 
 const page = () => {
   return (
     <>
       <main>
-      <Flex
-        direction='column'
-        className='w-full h-auto py-5'>
-        <h1 className='font-bold text-2xl'>
-          近くのユーザー
-        </h1>
-        <div className='text-4xl bg-gray-200'>a</div>
-      </Flex>
+        <Flex
+          direction='column'
+          className='w-full h-auto p-3'>
+          <h2>
+            近くのユーザー
+          </h2>
+          <UserList />
+        </Flex>
       </main>
       <Footer />
     </>
