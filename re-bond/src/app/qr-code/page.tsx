3fd5d95013afb843ffc50459c98bd9894dd389cc
KentM <kent20020324@gmail.com>
2023-08-24 17:42:32 +0900

    QRコード画面にQRコードが表示されるように変更しました。

--- re-bond/src/app/qr-code/page.tsx
+++ re-bond/src/app/qr-code/page.tsx
@@ -1,22 +1,29 @@
+"use client"
 import { Flex } from '@/components/elements/box/Flex'
 import Footer from '@/components/layouts/footer/Footer'
 import React from 'react'
+import QRCode from 'qrcode.react';
 
-const page = () => {
+const QRCodePage = () => {
+  const qrCodeValue = 'https://google.com'; // QR コードに表示する値を設定
+
   return (
     <>
       <main>
         <Flex
           direction='column'
-          className='w-full h-auto p-3'>
-          <h2>
-            QRコード
-          </h2>
+          align='center'
+          className='w-full h-auto p-3'
+        >
+          <h2>QRコード</h2>
+          <div className='mt-3'>
+            <QRCode value={qrCodeValue} size={200} />
+          </div>
         </Flex>
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </>
-  )
-}
+  );
+};
 
-export default page
+export default QRCodePage;
