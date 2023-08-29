7519362027bd5f4b1b4523185187b920cb496c07
coa3 <lovecoal928@gmail.com>
2023-08-29 13:32:49 +0900

    feat: ミッション達成条件の追加

--- re-bond/src/app/missions/components/Require.tsx
+++ re-bond/src/app/missions/components/Require.tsx
@@ -1,5 +1,8 @@
+'use client'
+
 import { Flex } from '@/components/elements/box/Flex'
 import React, { useState } from 'react'
+import { ref, uploadBytes } from 'firebase/storage'
 
 type Props = {
   require: boolean
@@ -8,14 +11,16 @@ type Props = {
 const Require = ({require}: Props) => {
   const [imagePath, setImagePath] = useState<string | null>()
 
-  const handleUpload = async(e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0]
-    if (file) {
-      setImagePath(file.name)
-      console.log('画像をアップロードしました')
-    }
-    
-  }
+  // const handleUpload = async(e: React.ChangeEvent<HTMLInputElement>) => {
+  //   const file = e.target.files?.[0]
+  //   if(file){
+  //     const storageRef = ref(storage, 'image/' + file.name)
+  //     uploadBytes(storageRef, file ).then((snapshot) => {
+  //       console.log('hogehoge')
+  //     })
+
+  //   }
+  // }
   return (
   <>
     <Flex
@@ -34,15 +39,16 @@ const Require = ({require}: Props) => {
         align='center'
         className='border-2 border-baseBlue rounded-3xl p-10 w-full text-2xl mb-5'>
         <input
-            id='requirements'
-            type='file'
-            accept='.png .jpeg .jpg'
-            className='hidden'
-            onChange={handleUpload}
-            />
-            <label htmlFor='requirements'>
-              写真をアップロードしよう
-            </label>
+          id='requirements'
+          type='file'
+          accept='.png, .jpeg, .jpg'
+          className='hidden'
+        />
+        <label
+          htmlFor='requirements'
+          className='bg-baseBlue rounded-full text-white p-3 w-[90%] text-xl text-center cursor-pointer'>
+          写真をアップロード
+        </label>
       </Flex>
     }
   </>
