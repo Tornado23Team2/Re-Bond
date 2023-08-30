918960a2fb3b2a0425391d5415291f422c459c7d
Kazuya <okz21@icloud.com>
2023-08-30 22:25:38 +0900

    Topページの作成,このページからログインと新規登録画面へ遷移することができる。

--- re-bond/src/app/page.tsx
+++ re-bond/src/app/page.tsx
@@ -1,16 +1,33 @@
 import React from 'react'
 import { Flex } from "@/components/elements/box/Flex"
+import Image from "next/image";
 import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className='p-0'>
-      <Flex
+    <main className='p-0 m-0 '>
+      <Flex 
         justify="center"
-        className="w-full h-full items-center text-3xl font-bold underline">
-        <Link href='/home'>
-          Hello world!
-        </Link>
+        align="center"
+        direction="column" 
+        className="w-full h-screen">
+          <Image
+          src={"/header/headerLogo.svg"}
+          width={258}
+          height={97}
+          alt="re:bond"
+          className="mb-16"
+        />
+         <Link
+            href={"/login"} 
+            className="w-1/2 bg-baseBlue text-white p-3 rounded-md text-center  active:bg-blue-800 mb-4">
+            ログイン  
+          </Link>
+         <Link
+            href={"/Registration"} 
+            className="w-1/2 bg-indigo-600 bg-baseBlue text-white p-3 rounded-md text-center active:bg-blue-800">
+            新規登録  
+          </Link>
       </Flex>
     </main>
   );
