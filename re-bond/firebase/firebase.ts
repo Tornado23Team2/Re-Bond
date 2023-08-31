22052aebc9a21e9a45897b353abd16acc7d9de03
coa3 <lovecoal928@gmail.com>
2023-08-31 23:15:25 +0900

    fix: firebaseAuthの設定ミスによるFirebaseErrorの修正

--- re-bond/firebase/firebase.ts
+++ re-bond/firebase/firebase.ts
@@ -1,11 +1,10 @@
-"use client";
+"use client"
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app"
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { collection, getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { collection, getFirestore } from "firebase/firestore"
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -18,13 +17,12 @@ const firebaseConfig = {
   messagingSenderId: process.env.MESSAGINGSENDERID,
   appId: process.env.APPID,
   measurementId: process.env.MEASUREMENTID,
-};
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const collectionRef = collection(db, "mission_table");
-const auth = getAuth(app);
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+const collectionRef = collection(db, "mission_table")
 
-export default app;
-export { db, collectionRef, auth };
+export default app
+export { db, collectionRef }
